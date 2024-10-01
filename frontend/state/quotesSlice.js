6e3e49b27d3fc998ebaf3d8f9cf5150bc8499fa8
francesco damore <frandamore11@gmail.com>
2024-09-30 19:48:17 -0600

    setup store, slice, and provider

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,28 +1,71 @@
 // ✨ create your `quotesSlice` in this module
+import { createSlice } from '@reduxjs/toolkit'
 
 let id = 1
 const getNextId = () => id++
-const initialState = {
-  displayAllQuotes: true,
-  highlightedQuote: null,
-  quotes: [
-    {
-      id: getNextId(),
-      quoteText: "Don't cry because it's over, smile because it happened.",
-      authorName: "Dr. Seuss",
-      apocryphal: false,
+const slice = createSlice({
+
+  name: 'quotes',
+  initialState: {
+    list: [
+      {
+        id: getNextId(),
+        quoteText: "Don't cry because it's over, smile because it happened.",
+        authorName: "Dr. Seuss",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "So many books, so little time.",
+        authorName: "Frank Zappa",
+        apocryphal: false,
+      },
+      {
+        id: getNextId(),
+        quoteText: "Be yourself; everyone else is already taken.",
+        authorName: "Oscar Wilde",
+        apocryphal: false,
+      },
+    ],
+  },
+  reducers: {
+    displayAllQuotes(state, action){
+
     },
-    {
-      id: getNextId(),
-      quoteText: "So many books, so little time.",
-      authorName: "Frank Zappa",
-      apocryphal: false,
-    },
-    {
-      id: getNextId(),
-      quoteText: "Be yourself; everyone else is already taken.",
-      authorName: "Oscar Wilde",
-      apocryphal: false,
-    },
-  ],
-}
+    highlightedQuote(state,action){
+
+    }
+  }
+})
+
+
+// const initialState = {
+//   displayAllQuotes: true,
+//   highlightedQuote: null,
+//   quotes: [
+//     {
+//       id: getNextId(),
+//       quoteText: "Don't cry because it's over, smile because it happened.",
+//       authorName: "Dr. Seuss",
+//       apocryphal: false,
+//     },
+//     {
+//       id: getNextId(),
+//       quoteText: "So many books, so little time.",
+//       authorName: "Frank Zappa",
+//       apocryphal: false,
+//     },
+//     {
+//       id: getNextId(),
+//       quoteText: "Be yourself; everyone else is already taken.",
+//       authorName: "Oscar Wilde",
+//       apocryphal: false,
+//     },
+//   ],
+// }
+
+export default slice.reducer
+
+export const {
+  displayAllQuotes, highlightedQuote
+} = slice.actions
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,1 +1,10 @@
 // ✨ create your `store` in this module
+import { configureStore } from '@reduxjs/toolkit'
+import reducer from './quotesSlice'
+
+export const store = configureStore({
+  reducer: {
+    quotes: reducer,
+    // other reducers would go here
+  }
+})
